00aaf7d5fd2ec025e72e74054cb6feb267981d03
Sbat2000 <103517615+Sbat2000@users.noreply.github.com>
2023-01-06 19:46:11 +0800

    add: QuestionFactory, Models (QSVM, QRVM, QQ)

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Александр Гарипов on 06.01.2023.
+//
+
+import Foundation
+
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+}
---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Александр Гарипов on 06.01.2023.
+//
+
+import Foundation
+import UIKit
+
+struct QuizStepViewModel {
+  let image: UIImage
+  let question: String
+  let questionNumber: String
+}
+
